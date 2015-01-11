6142ab7bd59fcf881db3229f6d0ffe7cdd4d5963
unknown <renurozera@gmail.com>
2015-01-11 23:10:01 +0530

    get method

--- a.php
+++ a.php
@@ -1,7 +1,6 @@
 <?php
-header("Access-Control-Allow-Origin: *"); 
-    if (isset($_POST['var_PHP_data'])) {
-      $var1=$_POST['var_PHP_data'];
-	  echo($var1);
-    } else { echo "failed";}
+ if($_GET["time"])
+  {
+     echo $_GET['time'];
+  }
 ?>
